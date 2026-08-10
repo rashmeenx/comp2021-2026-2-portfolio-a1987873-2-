a00028f493bd6f690eb81e9091d2499907b3a03a
Rashmeen Islam <rashmeenislam1@gmail.com>
2026-08-11 03:29:04 +0930

    Task 3: unit testing

---
+++ Workshop1/PracB/TodoApp.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿public class PersonTests
+{
+    [Fact]
+    public void FullName_ReturnsExpectedFormat()
+    {
+        Person person = new Person("Rashmeen", "Islam", 20);
+
+        string result = person.FullName();
+
+        Assert.Equal("Islam, Rashmeen", result);
+    }
+
+    [Fact]
+    public void IsAdult_ReturnsTrue_WhenAge18OrMore()
+    {
+        Person person = new Person("Rashmeen", "Islam", 20);
+
+        bool result = person.IsAdult();
+
+        Assert.True(result);
+    }
+}
