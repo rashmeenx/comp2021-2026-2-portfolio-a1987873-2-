c665ce1159b6c1a8d5503c12349cfb37f0a59c40
Rashmeen Islam <rashmeenislam1@gmail.com>
2026-08-11 04:08:09 +0930

    Finished task 2

--- Workshop2/PracA/PayrollApp/Payroll.cs
+++ Workshop2/PracA/PayrollApp/Payroll.cs
@@ -4,34 +4,77 @@ public class Payroll
     private decimal rate;
     private decimal taxRate;
 
+    public double Hours
+    {
+        get
+        {
+            return hours;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(
+                    "Hours cannot be negative."
+                );
+            }
+
+            hours = value;
+        }
+    }
+
+    public decimal Rate
+    {
+        get
+        {
+            return rate;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(
+                    "Rate cannot be negative."
+                );
+            }
+
+            rate = value;
+        }
+    }
+
+    public decimal TaxRate
+    {
+        get
+        {
+            return taxRate;
+        }
+
+        set
+        {
+            if (value < 0 || value > 1)
+            {
+                throw new ArgumentException(
+                    "Tax rate must be between 0 and 1."
+                );
+            }
+
+            taxRate = value;
+        }
+    }
+
     public Payroll(double hours, decimal rate, decimal taxRate)
     {
-        if (hours < 0)
-        {
-            throw new ArgumentException("Hours cannot be negative.");
-        }
-
-        if (rate < 0)
-        {
-            throw new ArgumentException("Rate cannot be negative.");
-        }
-
-        if (taxRate < 0 || taxRate > 1)
-        {
-            throw new ArgumentException(
-                "Tax rate must be between 0 and 1."
-            );
-        }
-
-        this.hours = hours;
-        this.rate = rate;
-        this.taxRate = taxRate;
+        Hours = hours;
+        Rate = rate;
+        TaxRate = taxRate;
     }
 
     public decimal CalculateNetPay()
     {
-        decimal grossPay = (decimal)hours * rate;
-        decimal tax = grossPay * taxRate;
+        decimal grossPay = (decimal)Hours * Rate;
+        decimal tax = grossPay * TaxRate;
         decimal netPay = grossPay - tax;
 
         return netPay;
@@ -39,13 +82,6 @@ public class Payroll
 
     public void ChangeTaxRate(decimal newTaxRate)
     {
-        if (newTaxRate < 0 || newTaxRate > 1)
-        {
-            throw new ArgumentException(
-                "Tax rate must be between 0 and 1."
-            );
-        }
-
-        taxRate = newTaxRate;
+        TaxRate = newTaxRate;
     }
 }
