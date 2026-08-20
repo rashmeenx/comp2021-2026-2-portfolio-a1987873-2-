3d90eaa3a95453c9147ba18482274f976215d545
Rashmeen Islam <rashmeenislam1@gmail.com>
2026-08-20 15:58:02 +0930

    Polymorphism demonstrated.

--- Workshop3/PracB/Employee/Program.cs
+++ Workshop3/PracB/Employee/Program.cs
@@ -1,19 +1,24 @@
-﻿FullTimeEmployee fullTimeEmployee =
-    new FullTimeEmployee("Alice", 80000m);
+﻿List<Employee> employees = new List<Employee>();
 
-Contractor contractor =
-    new Contractor("Bob", 50m, 40m);
+employees.Add(new FullTimeEmployee("Alice", 80000m));
+employees.Add(new Contractor("Bob", 50m, 40m));
 
-Console.WriteLine("FULL-TIME EMPLOYEE");
-Console.WriteLine(fullTimeEmployee.GenerateReport());
-Console.WriteLine(
-    $"CalculatePay(): ${fullTimeEmployee.CalculatePay():F2}"
-);
+foreach (Employee employee in employees)
+{
+    decimal pay = employee.CalculatePay();
 
-Console.WriteLine();
+    decimal tax = 0;
 
-Console.WriteLine("CONTRACTOR");
-Console.WriteLine(contractor.GenerateReport());
-Console.WriteLine(
-    $"CalculatePay(): ${contractor.CalculatePay():F2}"
-);
+    if (employee is FullTimeEmployee fullTimeEmployee)
+    {
+        tax = fullTimeEmployee.AnnualSalary * Employee.TaxRate;
+    }
+    else if (employee is Contractor contractor)
+    {
+        tax = contractor.Rate * contractor.Hours * Employee.TaxRate;
+    }
+
+    Console.WriteLine(
+        $"{employee.Name}: Pay ${pay:F2}. Tax ${tax:F2}."
+    );
+}
