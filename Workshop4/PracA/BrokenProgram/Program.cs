51c6a77980227a352c1a5796ec4d13d533bb6b76
Rashmeen Islam <rashmeenislam1@gmail.com>
2026-08-26 12:34:51 +0930

    Workshop4a task 1: refactored code.

---
+++ Workshop4/PracA/BrokenProgram/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Topic4a;
+public static class BrokenProgram
+{
+    static void Main(string[] args)
+    {
+    const string input = """
+        23
+        +
+        77
+        +
+        3
+        +
+        457
+        *
+        2
+        """;
+    int total = 0;
+    List<int> items = [];
+    List<string> symbols = [];
+    
+    string[] lines = input.Split(
+        '\n',
+        StringSplitOptions.RemoveEmptyEntries
+    );
+
+    for (int i = 0; i < lines.Length; i += 2)
+    {
+        items.Add(int.Parse(lines[i].Trim()));
+        if (i != 0)
+        {
+        symbols.Add(lines[i - 1].Trim());
+        }
+    }
+    
+
+    foreach (var (i, value) in items.Index())
+    {
+        if (i == 0) { total = value; }
+        else if (symbols[i - 1] == "*")
+        {
+        total *= value;
+        }
+        else
+        {
+        total += value;
+        }
+    }
+    Console.WriteLine("Total was: " + total);
+    Console.WriteLine("Expected total was: 1120");
+    Console.WriteLine(total == 1120 ? "Your answer was RIGHT!" : "Your answer was WRONG!\nGo and fix it.");
+    }
+}
