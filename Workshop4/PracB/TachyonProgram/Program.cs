5d383a3ef4ec1ced48b8fca1f7453765af478016
Rashmeen Islam <rashmeenislam1@gmail.com>
2026-08-30 18:07:43 +0930

    refactoring the code

--- Workshop4/PracB/TachyonProgram/Program.cs
+++ Workshop4/PracB/TachyonProgram/Program.cs
@@ -1,119 +1,118 @@
-﻿namespace AdventDay7;
+﻿using System.Text;
 
-using System.IO.Pipelines;
-using System.Text;
+namespace TachyonProgram
+{
+
 /// <summary>
 /// Class <c>Program</c> contains the magical tachyon manifold.
 /// </summary>
-public static class Program
-{
-
-    //This is a singleline comment.
-    //TODO: Change hardcoded string for the input file to one on your PC.
-    public static string FileToRead { get; set; } = "./Example.txt";
-
-    /// <summary>
-    /// This method processes the start of the tachyon manifold
-    /// <example>
-    /// For example:
-    /// <code>
-    /// ProcessStart(0, 0, taychonGrid);
-    /// </code>
-    /// results in <c>'|'</c>being added to the grid point (0,1)
-    /// </example>
-    /// </summary>
-    /// <param name="posX"></param>
-    /// <param name="posY"></param>
-    /// <param name="tachyonGrid"></param>
-    public static void processStart(int posX, int posY, List<string> tachyonGrid)
+    public static class Program
     {
-        string line = tachyonGrid[posY + 1];
-        StringBuilder sb = new StringBuilder(line);
-        sb[posX] = '|';
-        line = sb.ToString();
-        tachyonGrid[posY + 1] = line;
-    }
 
-    /*
-    This is a multiline comment
-    TODO: ProcessSplitter should return an int to add to the total, but it currently returns nothing.
-    */
-    public static void processSplitter(int posX, int posY, List<string> tachyonGrid)
-    {
-        string line = tachyonGrid[posY];
-        StringBuilder sb = new StringBuilder(line);
-        if (tachyonGrid[posY - 1][posX] == '|')
+        //This is a singleline comment.
+        //TODO: Change hardcoded string for the input file to one on your PC.
+        public static string FileToRead { get; set; } = "./Example.txt";
+
+
+        /// <summary>
+        /// Starts the tachyon beam by placing a '|' below the start symbol 'S'.
+        /// </summary>
+        /// <param name="posX">The horizontal position of 'S'.</param>
+        /// <param name="posY">The vertical position of 'S'.</param>
+        /// <param name="tachyonGrid">The tachyon grid.</param>
+        public static void ProcessStart(int posX, int posY, List<string> tachyonGrid)
         {
-            sb[posX - 1] = '|';
-            sb[posX + 1] = '|';
-            line = sb.ToString();
-            tachyonGrid[posY] = line;
-        }
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="posX"></param>
-    /// <param name="posY"></param>
-    /// <param name="tachyonGrid"></param>
-    public static void processEmpty(int posX, int posY, List<string> tachyonGrid)
-    {
-        string line = tachyonGrid[posY];
-        StringBuilder sb = new StringBuilder(line);
-        if (tachyonGrid[posY - 1][posX] == '|')
-        {
+            string line = tachyonGrid[posY + 1];
+            StringBuilder sb = new(line);
             sb[posX] = '|';
             line = sb.ToString();
-            tachyonGrid[posY] = line;
+            tachyonGrid[posY + 1] = line;
         }
-    }
 
-    public static void Main(string[] args)
-    {
-      
-        var sr = new StreamReader(FileToRead);
-        string srText = sr.ReadToEnd();
-        int totalCount = 0;
+        /// <summary>
+        /// Creates beams to the left and right when a beam reaches a splitter '^'.
+        /// </summary>
+        /// <param name="posX">The horizontal position of the splitter.</param>
+        /// <param name="posY">The vertical position of the splitter.</param>
+        /// <param name="tachyonGrid">The tachyon grid.</param>
 
-        List<string> symbols = [.. srText.Split("\n")];
-        List<string> tachyonGrid = symbols[..^1];
-
-        Console.WriteLine("Begin Tachyon Manifold start state");
-        foreach (var item in tachyonGrid)
+        public static void ProcessSplitter(int posX, int posY, List<string> tachyonGrid)
         {
-            Console.WriteLine(item);
-        }
-        Console.WriteLine("End Tachyon Manifold start state");
-
-        for (int posY = 0; posY < tachyonGrid.Count; posY++)
-        {
-            var item = tachyonGrid[posY];
-            foreach (var (posX, character) in item.Index())
+            string line = tachyonGrid[posY];
+            StringBuilder sb = new(line);
+            if (tachyonGrid[posY - 1][posX] == '|')
             {
-                if (character == 'S')
-                {
-                    processStart(posX, posY, tachyonGrid);
-                }
-                else if (character == '^')
-                {
-                    processSplitter(posX, posY, tachyonGrid);
-                }
-                else if (posY != 0)
-                {
-                    processEmpty(posX, posY, tachyonGrid);
-                }
+                sb[posX - 1] = '|';
+                sb[posX + 1] = '|';
+                line = sb.ToString();
+                tachyonGrid[posY] = line;
             }
         }
-        Console.WriteLine($"Begin Taychon Manifold end state");
 
-        foreach (var item in tachyonGrid)
+        /// <summary>
+        /// Continues a beam downward through an empty space.
+        /// </summary>
+        /// <param name="posX">The horizontal position being checked.</param>
+        /// <param name="posY">The vertical position being checked.</param>
+        /// <param name="tachyonGrid">The tachyon grid.</param>
+        public static void ProcessEmpty(int posX, int posY, List<string> tachyonGrid)
         {
-            Console.WriteLine(item);
+            string line = tachyonGrid[posY];
+            StringBuilder sb = new(line);
+            if (tachyonGrid[posY - 1][posX] == '|')
+            {
+                sb[posX] = '|';
+                line = sb.ToString();
+                tachyonGrid[posY] = line;
+            }
         }
-        Console.WriteLine($"End Taychon Manifold end state");
-        Console.WriteLine($"Total Tachyon Manifold splits were: {totalCount}");
-        Console.WriteLine($"If using Example.txt, your total should be 21");
-        Console.WriteLine($"If using Example.txt, your total is {(totalCount == 21 ? "RIGHT": "WRONG")}");
+
+        public static void Main(string[] args)
+        {
+        
+            var sr = new StreamReader(FileToRead);
+            string srText = sr.ReadToEnd();
+            int totalCount = 0;
+
+            List<string> symbols = [.. srText.Split("\n")];
+            List<string> tachyonGrid = symbols[..^1];
+
+            Console.WriteLine("Begin Tachyon Manifold start state");
+            foreach (var item in tachyonGrid)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("End Tachyon Manifold start state");
+
+            for (int posY = 0; posY < tachyonGrid.Count; posY++)
+            {
+                var item = tachyonGrid[posY];
+                foreach (var (posX, character) in item.Index())
+                {
+                    if (character == 'S')
+                    {
+                        ProcessStart(posX, posY, tachyonGrid);
+                    }
+                    else if (character == '^')
+                    {
+                        ProcessSplitter(posX, posY, tachyonGrid);
+                    }
+                    else if (posY != 0)
+                    {
+                        ProcessEmpty(posX, posY, tachyonGrid);
+                    }
+                }
+            }
+            Console.WriteLine($"Begin Taychon Manifold end state");
+
+            foreach (var item in tachyonGrid)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"End Taychon Manifold end state");
+            Console.WriteLine($"Total Tachyon Manifold splits were: {totalCount}");
+            Console.WriteLine($"If using Example.txt, your total should be 21");
+            Console.WriteLine($"If using Example.txt, your total is {(totalCount == 21 ? "RIGHT": "WRONG")}");
+        }
     }
 }
