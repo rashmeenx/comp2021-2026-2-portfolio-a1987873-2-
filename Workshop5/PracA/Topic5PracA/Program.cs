c5899e4ccccc1a3b67720254b7cd490a252a9fb4
Rashmeen Islam <rashmeenislam1@gmail.com>
2026-09-01 11:10:33 +0930

    Complete Task 1: workshop5a.

---
+++ Workshop5/PracA/Topic5PracA/Program.cs
@@ -0,0 +1,73 @@
+﻿string[] names =
+{
+    "Zara",
+    "Ben",
+    "Emily",
+    "Alice",
+    "Christopher",
+    "Dana",
+    "Jack",
+    "Gabriella",
+    "Frank",
+    "Henry"
+};
+
+Console.WriteLine("Main string: ");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+string LongestName = names[0];
+
+foreach (string name in names)
+{
+    if (name.Length > LongestName.Length)
+    {
+        LongestName = name;
+    }
+}
+
+Console.WriteLine($"Longest name: {LongestName}");
+
+string ShortestName = names[0];
+
+foreach (string name in names)
+{
+    if (name.Length < ShortestName.Length)
+    {
+        ShortestName = name;
+    }
+}
+
+Console.WriteLine($"Shortest name: {ShortestName}");
+
+
+Console.WriteLine("\nBefore sorting:");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Array.Sort(names);
+
+Console.WriteLine("\nAfter sorting:");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine("\nBefore Reversing:");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Array.Reverse(names);
+
+Console.WriteLine("\nAfter Reversing:");
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
