886f2610cff188effb88dc988202e57320ae014a
Rashmeen Islam <rashmeenislam1@gmail.com>
2026-09-09 11:38:52 +0930

    Stack Task1:workshop6A.

---
+++ Workshop6/PracA/UsingStack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class UsingStack
+{
+    public static void Main()
+    {
+        Stack<string> undoHistory = new Stack<string>(); //using undoHistory(camelcase) as it is local variable
+        undoHistory.Push("Typed Text");
+        undoHistory.Push("Inserted Image");
+        undoHistory.Push("Changed Colour");
+
+        foreach (string history in undoHistory)
+        {
+            Console.WriteLine(history);
+        }
+        
+        Console.WriteLine("\nPopping '{0}'", undoHistory.Pop());
+        foreach (string history in undoHistory)
+        {
+            Console.WriteLine(history);
+        }
+
+
+        Console.WriteLine("\nPeek at next item to destack: {0}",
+            undoHistory.Peek());
+        foreach (string history in undoHistory)
+        {
+            Console.WriteLine(history);
+        }
+
+        Console.WriteLine("\nPopping '{0}'", undoHistory.Pop());
+        foreach (string history in undoHistory)
+        {
+            Console.WriteLine(history);
+        }
+    }
+
+    
+}
